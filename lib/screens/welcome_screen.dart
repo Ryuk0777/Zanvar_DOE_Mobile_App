5a819859ccd3ecee521a4b3ba7bcbdd2e7d73a71
Ryuk0777 <114020461+Ryuk0777@users.noreply.github.com>
2025-03-21 18:15:48 +0530

    Add WelcomeScreen

---
+++ lib/screens/welcome_screen.dart
@@ -0,0 +1,104 @@
+import 'package:flutter/material.dart';
+
+class WelcomeScreen extends StatelessWidget {
+  const WelcomeScreen({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Stack(
+        fit: StackFit.expand,
+        children: [
+
+          Image.asset(
+            "assets/bg.jpeg",
+            fit: BoxFit.cover,
+          ),
+
+          Container(
+            color: Colors.black.withValues(alpha: 0.3),
+          ),
+
+          Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              Spacer(),
+              Text(
+                "Welcome to DOE app",
+                style: TextStyle(
+                  fontSize: 22,
+                  fontWeight: FontWeight.bold,
+                  color: Colors.white
+                ),
+              ),
+
+              SizedBox(height: 8),
+
+              Text(
+                "Design, Test, and Improve Processes for\nSuperior Results",
+                textAlign: TextAlign.center,
+                style: TextStyle(
+                  fontSize: 14,
+                  color: Colors.white,
+                ),
+              ),
+
+              SizedBox(height: 20),
+
+              Image.asset(
+                'assets/z-logo 1.png',
+                width: 250,
+              ),
+
+              Spacer(),
+
+              Padding(
+                padding: const EdgeInsets.symmetric(horizontal: 20),
+                child: Column(
+                  children: [
+                    ElevatedButton(
+                      style: ElevatedButton.styleFrom(
+                        backgroundColor: Colors.white,
+                        minimumSize: Size(double.infinity, 50),
+                        shape: RoundedRectangleBorder(
+                          borderRadius: BorderRadius.circular(10),
+                        ),
+                      ),
+                      onPressed: () {
+                        Navigator.pushNamed(context, '/createAccount');
+                      },
+                      child: Text(
+                        "Create Account",
+                        style: TextStyle(
+                          color: Colors.black,
+                          fontSize: 16,
+                        ),
+                      ),
+                    ),
+                    SizedBox(height: 10),
+                    GestureDetector(
+                      onTap: () {
+                        Navigator.pushNamed(context, '/login'); 
+                      },
+                      child: Text(
+                        "Already have an account? Log in",
+                        style: TextStyle(
+                          color: Colors.white,
+                          fontSize: 14,
+                          fontWeight: FontWeight.bold,
+                        ),
+                      ),
+                    ),
+                    SizedBox(height: 20),
+                  ],
+                ),
+              ),          
+
+            ],
+          )
+
+        ],
+      ),
+    );
+  }
+}
